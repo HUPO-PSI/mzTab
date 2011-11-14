cbd92d1d9a87874a188bdef67c423f08c32299b7
jgriss8@yahoo.com <jgriss8@yahoo.com@978631d8-7852-4d02-a635-ba49c394ae06>
2011-11-14 11:28:46 +0000

    [maven-release-plugin]  copy for tag mztab-java-1.0.1

---
+++ parser/mztab-java/tags/mztab-java-1.0.1/src/main/java/uk/ac/ebi/pride/mztab_java/MzTabParsingException.java
@@ -0,0 +1,22 @@
+package uk.ac.ebi.pride.mztab_java;
+
+@SuppressWarnings("serial")
+public class MzTabParsingException extends Exception {
+
+	public MzTabParsingException() {
+		
+	}
+
+	public MzTabParsingException(String arg0) {
+		super(arg0);
+	}
+
+	public MzTabParsingException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public MzTabParsingException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+}
