4c79e33179242632a9c4caab101bfa1df72f762d
Johannes Griss <jgriss@users.noreply.github.com>
2012-04-24 12:52:44 +0000

    Replaced "\n" with MzTabFile.EOL in all test cases.

--- parser/mztab-java/trunk/src/test/java/uk/ac/ebi/pride/mztab_java/model/UnitTest.java
+++ parser/mztab-java/trunk/src/test/java/uk/ac/ebi/pride/mztab_java/model/UnitTest.java
@@ -1,22 +1,23 @@
 package uk.ac.ebi.pride.mztab_java.model;
 
 import junit.framework.TestCase;
+import uk.ac.ebi.pride.mztab_java.MzTabFile;
 import uk.ac.ebi.pride.mztab_java.MzTabParsingException;
 
 public class UnitTest extends TestCase {
     private Unit unit = new Unit();
 
-    private static final String mzTabString = "MTD\tPRIDE_1234-title\tmzTab iTRAQ\n" +
-            "MTD\tPRIDE_1234-sample_processing[1]\t[MS,MS:1234,first step,]|[MS,MS:1235,describing first step,]\n" +
-            "MTD\tPRIDE_1234-contact[1]-name\tJohannes\n" +
-            "MTD\tPRIDE_1234-contact[2]-name\tMathias\n" +
-            "MTD\tPRIDE_1234-ms_file[1]-format\t[MS,MS:1000584,mzML file,]\n" +
-            "MTD\tPRIDE_1234-ms_file[1]-location\t/tmp/somefile.xml\n" +
-            "MTD\tPRIDE_1234-ms_file[1]-id_format\t[MS,MS:1001530,mzML unique identifier,]\n" +
-            "MTD\tPRIDE_1234-sub[1]-description\tHealthy human liver\n" +
-            "MTD\tPRIDE_1234-sub[1]-quantification_reagent\t[PRIDE,PRIDE:0000114,iTRAQ reagent 114,]\n" +
-            "MTD\tPRIDE_1234-sub[2]-description\tHuman hepatocellular carcinoma\n" +
-            "MTD\tPRIDE_1234-sub[2]-quantification_reagent\t[PRIDE,PRIDE:0000115,iTRAQ reagent 115,]\n";
+    private static final String mzTabString = "MTD\tPRIDE_1234-title\tmzTab iTRAQ" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-sample_processing[1]\t[MS,MS:1234,first step,]|[MS,MS:1235,describing first step,]" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-contact[1]-name\tJohannes" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-contact[2]-name\tMathias" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-ms_file[1]-format\t[MS,MS:1000584,mzML file,]" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-ms_file[1]-location\t/tmp/somefile.xml" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-ms_file[1]-id_format\t[MS,MS:1001530,mzML unique identifier,]" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-sub[1]-description\tHealthy human liver" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-sub[1]-quantification_reagent\t[PRIDE,PRIDE:0000114,iTRAQ reagent 114,]" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-sub[2]-description\tHuman hepatocellular carcinoma" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-sub[2]-quantification_reagent\t[PRIDE,PRIDE:0000115,iTRAQ reagent 115,]" + MzTabFile.EOL;
 
     public void testUnmarshall() {
         try {
