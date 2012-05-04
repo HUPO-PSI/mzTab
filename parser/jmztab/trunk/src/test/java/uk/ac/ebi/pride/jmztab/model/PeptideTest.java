3ef6a181fa2b705bf5b5eaac3dc1ca86402c5345
jgriss8@yahoo.com <jgriss8@yahoo.com@978631d8-7852-4d02-a635-ba49c394ae06>
2012-05-04 13:19:17 +0000

    Updated test cases to mzTab 0.9.2

--- parser/jmztab/trunk/src/test/java/uk/ac/ebi/pride/jmztab/model/PeptideTest.java
+++ parser/jmztab/trunk/src/test/java/uk/ac/ebi/pride/jmztab/model/PeptideTest.java
@@ -11,26 +11,26 @@ import uk.ac.ebi.pride.jmztab.util.TsvTableParser;
 
 public class PeptideTest extends TestCase {
 	public void testMarshallingUnmarshalling() {
-		String line1 = "PEP	NILN[14]ELFQR	gi|10181184	PRIDE_1234	true	NCBI NR	21.06.2011	[MS,MS:1001207,Mascot,]	[MS,MS:1001171,Mascot:score,35]	3	1[0.8]-UNIMOD:35	20.8	2	500	-	ms_file[1]:index=4	1.0	-	-	4491.221363	-	-	4491.221363	-	-	4491.221363	-	-				";
-		String line2 = "PEP	QG(my mod)IFQVVISR	gi|10181184	PRIDE_1234	false	NCBI NR	21.06.2011	[MS,MS:1001207,Mascot,]	[MS,MS:1001171,Mascot:score,40]	1	6[0.4]-UNIMOD:21	10.2	3	200	-	ms_file[2]:index=5	1.0	-	-	16500.75069	-	-	16500.75069	-	-	16500.75069	-	-				";
-		
-		TsvTableParser parser = new TsvTableParser("PEH	sequence	accession	unit_id	unique	database	database_version	search_engine	search_engine_score	reliability	modifications	retention_time	charge	mass_to_charge	uri	spectra_ref	peptide_abundance_sub[1]	peptide_abundance_stdev_sub[1]	peptide_abundance_std_error_sub[1]	peptide_abundance_sub[2]	peptide_abundance_stdev_sub[2]	peptide_abundance_std_error_sub[2]	peptide_abundance_sub[3]	peptide_abundance_stdev_sub[3]	peptide_abundance_std_error_sub[3]	peptide_abundance_sub[4]	peptide_abundance_stdev_sub[4]	peptide_abundance_std_error_sub[4]				");
+		String line1 = "PEP\tNILN[14]ELFQR\tgi|10181184\tPRIDE_1234\ttrue\tNCBI NR\t21.06.2011\t[MS,MS:1001207,Mascot,]\t[MS,MS:1001171,Mascot:score,35]\t3\t1[0.8]-UNIMOD:35\t20.8\t2\t500\tNA\tms_file[1]:index=4\t1.0\tNA\tNA\t4491.221363\tNA\tNA\t4491.221363\tNA\tNA\t4491.221363\tNA\tNA\t\t\t\t";
+		String line2 = "PEP\tQG(my mod)IFQVVISR\tgi|10181184\tPRIDE_1234\tfalse\tNCBI NR\t21.06.2011\t[MS,MS:1001207,Mascot,]\t[MS,MS:1001171,Mascot:score,40]\t1\t6[0.4]-UNIMOD:21\t10.2\t3\t200\tNA\tms_file[2]:index=5\t1.0\tNA\tNA\t16500.75069\tNA\tNA\t16500.75069\tNA\tNA\t16500.75069\tNA\tNA\t\t\t\t";
+
+		TsvTableParser parser = new TsvTableParser("PEH\tsequence\taccession\tunit_id\tunique\tdatabase\tdatabase_version\tsearch_engine\tsearch_engine_score\treliability\tmodifications\tretention_time\tcharge\tmass_to_charge\turi\tspectra_ref\tpeptide_abundance_sub[1]\tpeptide_abundance_stdev_sub[1]\tpeptide_abundance_std_error_sub[1]\tpeptide_abundance_sub[2]\tpeptide_abundance_stdev_sub[2]\tpeptide_abundance_std_error_sub[2]\tpeptide_abundance_sub[3]\tpeptide_abundance_stdev_sub[3]\tpeptide_abundance_std_error_sub[3]\tpeptide_abundance_sub[4]\tpeptide_abundance_stdev_sub[4]\tpeptide_abundance_std_error_sub[4]\t\t\t\t");
 		Map<String, String> parsedLine1 = parser.parseTableLine(line1);
 		Map<String, String> parsedLine2 = parser.parseTableLine(line2);
-		
+
 		try {
 			Peptide p1 = new Peptide(parsedLine1);
 			Peptide p2 = new Peptide(parsedLine2);
-			
+
 			assertNotNull(p1);
 			assertNotNull(p2);
-			
+
 			assertEquals("NILN[14]ELFQR", p1.getSequence());
 			assertEquals("NILNELFQR", p1.getCleanSequence());
-			
+
 			assertEquals("QG(my mod)IFQVVISR", p2.getSequence());
 			assertEquals("QGIFQVVISR", p2.getCleanSequence());
-			
+
 			assertEquals(line1.trim() + TableObject.EOL, p1.toMzTab(4, Collections.EMPTY_LIST));
 			assertEquals(line2.trim() + TableObject.EOL, p2.toMzTab(4, Collections.EMPTY_LIST));
 		} catch (MzTabParsingException e) {
