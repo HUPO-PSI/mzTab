1e1967c0fdfab4f30ef530bd1466f3d4ba7d6dd6
johannes.griss@gmail.com <johannes.griss@gmail.com@978631d8-7852-4d02-a635-ba49c394ae06>
2013-01-03 11:47:15 +0000

    Changed the string for missing values from "NA" to "null". Added support for "NaN" to represent calculation errors. Added version to maven-compiler-plugin.

--- parser/jmztab/trunk/src/test/java/uk/ac/ebi/pride/jmztab/model/SmallMoleculeTest.java
+++ parser/jmztab/trunk/src/test/java/uk/ac/ebi/pride/jmztab/model/SmallMoleculeTest.java
@@ -11,7 +11,7 @@ import uk.ac.ebi.pride.jmztab.util.TsvTableParser;
 
 public class SmallMoleculeTest extends TestCase {
 	public void testMarshallingUnmarshalling() {
-		String line1 = "SML\tThe molecule\tPRIDE_1234\tH2O\tNA\tNA\tA molecule that just rocks.\t143.45\t2\t10.0,28.8\t9606\tHuman (Homo sapiens)\tNA\tNA\t2\tNA\tms_file[4]:1\t[,,SpectraST,]\tNA\tNA\tThis col is cool!";
+		String line1 = "SML\tThe molecule\tPRIDE_1234\tH2O\tnull\tnull\tA molecule that just rocks.\t143.45\t2\t10.0,28.8\t9606\tHuman (Homo sapiens)\tnull\tnull\t2\tnull\tms_file[4]:1\t[,,SpectraST,]\tnull\tnull\tThis col is cool!";
 		
 		TsvTableParser parser = new TsvTableParser("SMH\tidentifier\tunit_id\tchemical_formula\tsmiles\tinchi_key\tdescription\tmass_to_charge\tcharge\tretention_time\ttaxid\tspecies\tdatabase\tdatabase_version\treliability\turi\tspectra_ref\tsearch_engine\tsearch_engine_score\tmodifications\topt_my_column");
 		Map<String, String> parsedLine1 = parser.parseTableLine(line1);
