d0a95dbfeef271a3a00e7205f2357fb682999aae
Johannes Griss <jgriss@users.noreply.github.com>
2013-02-16 17:54:18 +0000

    [maven-release-plugin]  copy for tag jmztab-1.0.9

---
+++ parser/jmztab/tags/jmztab-1.0.9/src/test/java/uk/ac/ebi/pride/jmztab/model/UnitTest.java
@@ -0,0 +1,66 @@
+package uk.ac.ebi.pride.jmztab.model;
+
+import junit.framework.TestCase;
+import uk.ac.ebi.pride.jmztab.MzTabFile;
+import uk.ac.ebi.pride.jmztab.MzTabParsingException;
+
+public class UnitTest extends TestCase {
+    private Unit unit = new Unit();
+
+    private static final String mzTabString = "MTD\tPRIDE_1234-title\tmzTab iTRAQ" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-sample_processing[1]\t[MS,MS:1234,first step,]|[MS,MS:1235,describing first step,]" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-contact[1]-name\tJohannes" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-contact[2]-name\tMathias" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-ms_file[1]-format\t[MS,MS:1000584,mzML file,]" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-ms_file[1]-location\t/tmp/somefile.xml" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-ms_file[1]-id_format\t[MS,MS:1001530,mzML unique identifier,]" + MzTabFile.EOL +
+	    "MTD\tPRIDE_1234-colunit-protein\tnum_peptides=[UO,UO:12345,Dalton,]" + MzTabFile.EOL +
+	    "MTD\tPRIDE_1234-colunit-peptide\tretention_time=[UO,UO:12345,seconds,]" + MzTabFile.EOL +
+	    "MTD\tPRIDE_1234-colunit-small_molecule\tretention_time=[UO,UO:12345,seconds,]" + MzTabFile.EOL +
+	    "MTD\tPRIDE_1234-sub[1]-description\tHealthy human liver" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-sub[1]-quantification_reagent\t[PRIDE,PRIDE:0000114,iTRAQ reagent 114,]" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-sub[2]-description\tHuman hepatocellular carcinoma" + MzTabFile.EOL +
+            "MTD\tPRIDE_1234-sub[2]-quantification_reagent\t[PRIDE,PRIDE:0000115,iTRAQ reagent 115,]" + MzTabFile.EOL;
+
+    public void testUnmarshall() {
+        try {
+            unit.unmarshall(mzTabString);
+
+            assertEquals("PRIDE_1234", unit.getUnitId());
+            assertEquals("mzTab iTRAQ", unit.getTitle());
+            assertEquals(2, unit.getContact().size());
+            assertEquals("Johannes", unit.getContact().get(0).getName());
+            assertEquals("Mathias", unit.getContact().get(1).getName());
+            assertEquals(2, unit.getSubsamples().size());
+            assertEquals("Healthy human liver", unit.getSubsamples().get(0).getDescription());
+            assertEquals("Human hepatocellular carcinoma", unit.getSubsamples().get(1).getDescription());
+            assertEquals("[PRIDE,PRIDE:0000114,iTRAQ reagent 114,]", unit.getSubsamples().get(0).getQuantificationReagent().toString());
+            assertEquals("[PRIDE,PRIDE:0000115,iTRAQ reagent 115,]", unit.getSubsamples().get(1).getQuantificationReagent().toString());
+	    
+	    assertEquals("[UO,UO:12345,Dalton,]", unit.getColunitProtein(ProteinTableField.NUM_PEPTIDES).toString());
+	    assertEquals("[UO,UO:12345,Dalton,]", unit.getColunitProtein("num_peptides").toString());
+	    
+	    assertEquals("[UO,UO:12345,seconds,]", unit.getColunitPeptide(PeptideTableField.RETENTION_TIME).toString());
+	    assertEquals("[UO,UO:12345,seconds,]", unit.getColunitPeptide("retention_time").toString());
+	    
+	    assertEquals("[UO,UO:12345,seconds,]", unit.getColunitSmallMolecule(SmallMoleculeTableField.RETENTION_TIME).toString());
+	    assertEquals("[UO,UO:12345,seconds,]", unit.getColunitSmallMolecule("retention_time").toString());
+	    
+            assertEquals(1, unit.getSampleProcessing().size());
+            assertEquals(2, unit.getSampleProcessing().get(0).size());
+            
+            MsFile msFile = unit.getMsFile(1);
+            
+            assertNotNull(msFile);
+            assertEquals("/tmp/somefile.xml", msFile.getLocation());
+            assertEquals("MS:1000584", msFile.getFormat().getAccession());
+
+            assertEquals(mzTabString, unit.toMzTab());
+
+        } catch (MzTabParsingException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+
+}
