1091d1e68a3d7ef58c8d38510548f53772ffc9ba
Nils Hoffmann <nils.hoffmann@isas.de>
2018-08-30 16:19:01 +0200

    Further updates and checks

--- validate.sh
+++ validate.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+V_VERSION="0.9.9"
+# download and unzip validator
 mkdir -p build/validation
 cd build/validation
-#rm -r *
-#wget http://central.maven.org/maven2/de/isas/mztab/jmztabm-cli/0.9.9/jmztabm-cli-0.9.9-bin.zip
-#unzip jmztabm-cli-0.9.9-bin.zip
+rm -r *
+wget http://central.maven.org/maven2/de/isas/mztab/jmztabm-cli/$V_VERSION/jmztabm-cli-$V_VERSION-bin.zip
+unzip jmztabm-cli-$V_VERSION-bin.zip
 cd jmztabm-cli
+
 # alternatives: Warn or Error
 V_LEVEL="Info"
 V_FAILED=()
+# run validation for all example files
 for i in $(find ../../../examples/2_0-Metabolomics_Draft/ -maxdepth 1 -iname '*.mztab'); do
   echo -e "################################################################################"
   echo -e "# Starting validation of $i on level "
-#  java -jar jmztabm-cli-0.9.9.jar -check inFile=$i -checkSemantic mappingFile=cv-mapping/mzTab-M-mapping.xml -level $V_LEVEL
-  java -jar jmztabm-cli-0.9.9.jar -check inFile=$i -level $V_LEVEL
+# semantic validation may take quite some time for larger files  
+#  java -jar jmztabm-cli-$V_VERSION.jar -check inFile=$i -checkSemantic mappingFile=cv-mapping/mzTab-M-mapping.xml -level $V_LEVEL
+  java -jar jmztabm-cli-$V_VERSION.jar -check inFile=$i -level $V_LEVEL
   if [ $? -ne 0 ];
   then
     echo -e "# Validation of file $i failed! Please check console output for errors!"
